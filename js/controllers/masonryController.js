0926281e91f398cf44dbe53ed6ddb6fde26b61bd
Travo100 <Travo100@gmail.com>
2015-08-11 20:15:52 -0700

    Still truckin

--- js/controllers/masonryController.js
+++ js/controllers/masonryController.js
@@ -5,62 +5,66 @@ angular
     
     function masonryController(masonryFactory, $window, $scope) {
         var vm = this,
-        	counter = 1,
-        	storiesNumber = 5,
-        	storiesList = [],
-        	getStories;
-        	vm.stories;
-        	vm.playVideo = playVideo;
-        	vm.showMoreStories = showMoreStories;
-        	vm.showButton = false;
+            counter = 1,
+            storiesNumber = 5,
+            storiesList = [];
+            vm.stories = [];
+            vm.playVideo = playVideo;
+            vm.showMoreStories = showMoreStories;
+            vm.showButton = false;
 
         storiesNumber = (function(){
-        	if ($window.innerWidth > 992) {
-        		return 12;
-        	} else if ($window.innerWidth > 480 && $window.innerWidth < 992) {
-        		return 8;
-        	} else {
-        		return 3;
-        	}
+            if ($window.innerWidth > 992) {
+                return 12;
+            } else if ($window.innerWidth > 480 && $window.innerWidth < 992) {
+                return 8;
+            } else {
+                return 3;
+            }
         }());
 
-        getStories = (function() {
-        	masonryFactory
-  				.getStories()
-  				.then(function(response){
-        			storiesList = response.data.stories;
-        			if(response.data.stories.length < storiesNumber) {
-        				vm.stories = response.data.stories;
-        			} else {
-        				vm.stories = response.data.stories.slice(0, storiesNumber);
-        				vm.showButton = true;
-        			}
-        		}, function(response){	
-        			console.log('error!', response);
-        		});
-        })();
+        function getAllStories() {
+            masonryFactory
+                .getStories()
+                .then(function(response){
+                    storiesList = response.data.stories;
+                    if(response.data.stories.length < storiesNumber) {
+                        vm.stories = response.data.stories;
+                        console.log(response.data.stories.length);
+                    } else {
+                        vm.stories = response.data.stories.slice(0, storiesNumber);
+                        vm.showButton = true;
+                        console.log(response.data.stories.length);
+                    }
+                }, function(response){  
+                    console.log('error!', response);
+                });
+        }
+        
+        getAllStories();
 
         function showMoreStories() {
-        	++counter;
-        	if (storiesList.length > storiesNumber * counter) {
-        		vm.stories = storiesList.slice(0, storiesNumber * counter);
-        	} else {
-        		vm.stories = storiesList.slice(0, storiesList.length);
-        		vm.showButton = false;
-        	}	
+            ++counter;
+            if (storiesList.length > storiesNumber * counter) {
+                // vm.stories = storiesList.slice(0, storiesNumber * counter);
+                vm.stories.push(storiesList.slice(storiesNumber * (counter - 1), storiesNumber * counter));
+            } else {
+                vm.stories = storiesList.slice(0, storiesList.length);
+                vm.showButton = false;
+            }   
         }
 
         function playVideo(videoId) {
-        	alert('playing video with ID ' + videoId);
+            alert('playing video with ID ' + videoId);
         }
 
 
         //grabbing this from the broadcast
         $scope.$on("scrollThreshold", function() {
-        	if (storiesList.length > storiesNumber * counter) {
-	        	showMoreStories();
-	        	$scope.$apply();
-        	}
+            if (storiesList.length > storiesNumber * counter) {
+                showMoreStories();
+                $scope.$apply();
+            }
         });
     }
 })();
